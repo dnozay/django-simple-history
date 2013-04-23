6d1c7836c4b931798d0374bcd9af9c2684893d50
dnozay <damien.nozay@gmail.com>
2013-04-22 22:27:03 -0700

    fix tests for applabel branch, add forgotten to add :/

---
+++ simple_history/tests/external/models.py
@@ -0,0 +1,8 @@
+
+from django.db import models
+from simple_history.models import HistoricalRecords
+
+class ExternalModel2(models.Model):
+    name = models.CharField(max_length=100)
+    history = HistoricalRecords()
+
